c2554c4859f7def1926cba60a0cc4885c9bc9a20
EC2 Default User <ec2-user@ip-172-31-30-155.ec2.internal>
2022-08-03 23:37:11 +0000

    run Kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ebunola/operationalizeml"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run operationalizeml\
+    --image=$dockerpath\
+    --port=80 --labels app=operationalizeml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward operationalizeml 8000:80
